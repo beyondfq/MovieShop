27e74baf0583da66bba5b7498975ae864f655280
beyondfq <qingfeng508508@gmail.com>
2022-07-22 01:19:47 -0400

    Added Fluent API for Movie entity

--- Infrustructure/Data/MovieShopDbContext.cs
+++ Infrustructure/Data/MovieShopDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using ApplicationCore.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,39 @@ namespace Infrustructure.Data
 
         }
         public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>(ConfigureMovie);
+        }
+
+        private void ConfigureMovie(EntityTypeBuilder<Movie> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Title).HasMaxLength(256);
+            builder.Property(m => m.Overview).HasMaxLength(4096);
+            builder.Property(m => m.Tagline).HasMaxLength(512);
+            builder.Property(m => m.ImdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.TmdbUrl).HasMaxLength(2084);
+            builder.Property(m => m.PosterUrl).HasMaxLength(2084);
+            builder.Property(m => m.BackdropUrl).HasMaxLength(2084);
+            builder.Property(m => m.OriginalLanguage).HasMaxLength(64);
+            builder.Property(m => m.UpdatedBy).HasMaxLength(512);
+            builder.Property(m => m.CreatedBy).HasMaxLength(512);
+
+            builder.Property(m => m.Price).HasColumnType("decimal(5, 2)").HasDefaultValue(9.9m);
+            builder.Property(m => m.Budget).HasColumnType("decimal(18, 4)").HasDefaultValue(9.9m);
+            builder.Property(m => m.Revenue).HasColumnType("decimal(18, 4)").HasDefaultValue(9.9m);
+
+            builder.Property(m => m.CreatedDate).HasDefaultValueSql("getdate()");
+
+            builder.Ignore(m => m.Rating);
+
+            builder.HasIndex(m => m.Title);
+            builder.HasIndex(m => m.Price);
+            builder.HasIndex(m => m.Revenue);
+            builder.HasIndex(m => m.Budget);
+        }
 
     }
 }
--- MovieShopMVC/Program.cs
+++ MovieShopMVC/Program.cs
@@ -1,7 +1,9 @@
 using ApplicationCore.Contracts.Repository;
 using ApplicationCore.Contracts.Services;
+using Infrustructure.Data;
 using Infrustructure.Repository;
 using Infrustructure.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IMovieService, MovieService>();
 builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 
-
+builder.Services.AddDbContext<MovieShopDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieShopDbConnection"));
+});
 
 var app = builder.Build();
 
