c4d8a20a5c9658ebcde31fbd8ca63f057264c792
beyondfq <qingfeng508508@gmail.com>
2022-07-25 00:17:26 -0400

    Added Cast Detail

--- Infrustructure/Repository/CastRepository.cs
+++ Infrustructure/Repository/CastRepository.cs
@@ -1,13 +1,30 @@
 ﻿using ApplicationCore.Contracts.Repository;
+using ApplicationCore.Entities;
+using Infrustructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrustructure.Repository
 {
     public class CastRepository: ICastRepository
     {
+        private readonly MovieShopDbContext _movieShopDbContext;
+
+        public CastRepository(MovieShopDbContext movieShopDbContext)
+        {
+            _movieShopDbContext = movieShopDbContext;
+        }
+
+        public async Task<Cast> GetById(int id)
+        {
+            var cast = await _movieShopDbContext.Casts
+                .Include(c => c.MoviesOfCast).ThenInclude(c => c.Movie)
+                .FirstOrDefaultAsync(c => c.Id == id);
+            return cast;
+        }
     }
 }
--- Infrustructure/Services/CastService.cs
+++ Infrustructure/Services/CastService.cs
@@ -1,4 +1,6 @@
-﻿using ApplicationCore.Contracts.Services;
+﻿using ApplicationCore.Contracts.Repository;
+using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,33 @@ namespace Infrustructure.Services
 {
     public class CastService: ICastService
     {
+        private readonly ICastRepository _castRepository;
+
+        public CastService(ICastRepository castRepository)
+        {
+            _castRepository = castRepository;
+        }
+
+        public async Task<CastModel> GetCastDetails(int castId)
+        {
+            var cast = await _castRepository.GetById(castId);
+            var castDetails = new CastModel
+            {
+                Id = cast.Id,
+                Name =  cast.Name,
+                ProfilePath = cast.ProfilePath
+            };
+
+            foreach (var movie in cast.MoviesOfCast)
+            {
+                castDetails.Movies.Add(new MovieCardModel
+                {
+                    Id = movie.MovieId,
+                    Title = movie.Movie.Title,
+                    PosterUrl = movie.Movie.PosterUrl
+                });
+            }
+            return castDetails;
+        }
     }
 }
