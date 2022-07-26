ea24585e34768ae0d746d8fa434f19a45d035128
beyondfq <qingfeng508508@gmail.com>
2022-07-26 15:49:54 -0400

    Added AccountController and Register model

--- MovieShopMVC/Controllers/AccountController.cs
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopMVC.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Register()
+        {
+            // show the View so that user can enter info and click on register button
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterModel model)
         {
             return View();
         }
