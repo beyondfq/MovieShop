8b7954dbf92352c998ad9070cbe462d318a8829e
beyondfq <qingfeng508508@gmail.com>
2022-07-28 00:22:23 -0400

    Added filters

--- MovieShopMVC/Controllers/UserController.cs
+++ MovieShopMVC/Controllers/UserController.cs
@@ -1,13 +1,17 @@
 ﻿using ApplicationCore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShopMVC.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         [HttpGet]
         public async Task<IActionResult> Purchases()
         {
+            // get all the movies purchased by user, user id
+            // httpcontext.user.claims and then call the database and get the information to the view
             return View();
         }
 
