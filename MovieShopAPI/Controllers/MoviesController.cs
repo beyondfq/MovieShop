1604e1c7cf9f2af9d37118a36bd1c023d5760466
beyondfq <qingfeng508508@gmail.com>
2022-08-02 21:49:26 -0400

    Added JWT Token Creation

---
+++ MovieShopAPI/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using ApplicationCore.Contracts.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        [Route("top-revenue")]
+        // Attribute Routing
+        public async Task<IActionResult> GetTopRevenueMovies()
+        {
+            var movies = await _movieService.GetTopRevenueMovies();
+
+            if(movies == null || !movies.Any())
+            {
+                // 404
+                return NotFound(new {errorMessage = "No Movies Found"});
+            }
+
+            return Ok(movies);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> GetMovie(int id)
+        {
+            var movie = await _movieService.GetMovieDetails(id);
+            if(movie==null)
+            {
+                return NotFound(new { errorMessage = $"No Movie Found for {id}" });
+            }
+
+            return Ok(movie);
+        }
+    }
+}
